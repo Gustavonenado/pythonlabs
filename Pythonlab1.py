b2361995c30e85999dc69f8249904876dc607bde
Gustavonenado <89265503+Gustavonenado@users.noreply.github.com>
2021-10-01 13:55:57 +0300

    Add files via upload

---
+++ Pythonlab1.py
@@ -0,0 +1,7 @@
+print("input first cathetus and press Enter:")
+a = float(input())
+print("input second cathetus and press Enter:")
+b = float(input())
+S = 0.5 * a * b 
+print("area of a triangle:")
+print(S)
