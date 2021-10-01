9511f1b69ea8dfe423dd2fda49f59222e7f0a6e4
Gustavonenado <89265503+Gustavonenado@users.noreply.github.com>
2021-10-01 13:46:24 +0300

    Add files via upload

---
+++ cpplab1.cpp
@@ -0,0 +1,16 @@
+﻿//#include <math.h> 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    
+    double a, b;  //катети
+    double S;     //площа трикутника
+    cout << "a="; cin >> a; //введення першого катета
+    cout << "b="; cin >> b; //введення другого катета 
+
+    S = 0.5 * a * b; //обчислення площі
+    cout << "\narea of a triangle S=" << S << endl; //виведення результату
+}
